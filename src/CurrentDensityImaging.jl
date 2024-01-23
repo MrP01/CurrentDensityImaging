e1477fd972cbbc05298d102b992583c9cc6a974e
MrP01 <peter@waldert.at>
2024-01-23 10:47:37 +0100

    switch to grid phantom, small fixes

--- src/CurrentDensityImaging.jl
+++ src/CurrentDensityImaging.jl
@@ -1,5 +1,50 @@
 module CurrentDensityImaging
-include("simulator/gridphantom.jl")
+include("gridphantom.jl")
+
+import KomaMRI
+import Lazy
+import LinearAlgebra
+import FastTransforms: fft, ifft
+import .GridPhantom as GP
+import GLMakie
+
+@kwdef struct CurrentDensityPhantom
+  pog::GP.PhantomOnAGrid  # phantom on grid
+  jx::Array{Float64,3} = ones(size(pog.ρ))
+  jy::Array{Float64,3} = zeros(size(pog.ρ))
+  jz::Array{Float64,3} = zeros(size(pog.ρ))
+end
+
+Lazy.@forward CurrentDensityPhantom.p KomaMRI.plot_phantom_map
+
+function plot_current_density(cdp::CurrentDensityPhantom)
+  flat = GP.to_flat_phantom(cdp.pog)
+  mask = cdp.pog.ρ .!= 0
+  GLMakie.arrows(flat.x, flat.y, flat.z, cdp.jx[mask], cdp.jy[mask], cdp.jz[mask], axis=(type=Axis3,))
+end
+
+function calculate_magnetic_field(cdp::CurrentDensityPhantom)
+  mu_0 = 1.0
+
+  g1(k) = -1.0im * k[1] ./ LinearAlgebra.norm(k) .^ 2
+  g2(k) = -1.0im * k[2] ./ LinearAlgebra.norm(k) .^ 2
+  g3(k) = -1.0im * k[3] ./ LinearAlgebra.norm(k) .^ 2
+
+  s = max(max(cdp.p.x...) - min(cdp.p.x...), max(cdp.p.y...) - min(cdp.p.y...), max(cdp.p.z...) - min(cdp.p.z...))
+  N = 100
+
+  k1 = fftfreq(N)
+  k = hcat(k1, k1, k1)
+
+  jx_interp = Interpolations.interpolate()
+  jx_on_grid = itp()
+
+  B1::Vector{Float64} = mu_0 * ifft(fft(cdp.jy) * g3.(k) - fft(cdp.jz) * g2.(k))
+  B2::Vector{Float64} = -mu_0 * ifft(fft(cdp.jx) * g3.(k) - fft(cdp.jz) * g1.(k))
+  B3::Vector{Float64} = mu_0 * ifft(fft(cdp.jx) * g2.(k) - fft(cdp.jy) * g1.(k))
+
+  return B1, B2, B3
+end
 
 greet() = print("Hello World!")
 end
--- src/gridphantom.jl
+++ src/gridphantom.jl
@@ -24,7 +24,7 @@ function read_grid_phantom_jemris(filename::String)
     T2s=1e-3 ./ data[4, :, :, :],
     Δw=data[5, :, :, :],
     Δx=vec(read(fid["sample/resolution"])) * 1e-3, # [m]
-    offset=vec(read(fid["sample/offset"])) * 1e-3 # [m]
+    offset=vec(read(fid["sample/offset"])) * 1e-3  # [m]
   )
 end
 
@@ -37,12 +37,12 @@ function to_flat_phantom(pog::PhantomOnAGrid)
   FOVy = (Y - 1) * pog.Δx[2] # [m]
   FOVz = (Z - 1) * pog.Δx[3] # [m]
 
-  xx = reshape((-FOVx/2:pog.Δx[1]:FOVx/2), :, 1, 1) #[(-FOVx/2:Δx[1]:FOVx/2)...;]
-  yy = reshape((-FOVy/2:pog.Δx[2]:FOVy/2), 1, :, 1) #[(-FOVy/2:Δx[2]:FOVy/2)...;;]
-  zz = reshape((-FOVz/2:pog.Δx[3]:FOVz/2), 1, 1, :) #[(-FOVz/2:Δx[3]:FOVz/2)...;;;]
-  x = xx * 1 .+ yy * 0 .+ zz * 0 .+ pog.offset[1]#spin x coordinates
-  y = xx * 0 .+ yy * 1 .+ zz * 0 .+ pog.offset[2]#spin y coordinates
-  z = xx * 0 .+ yy * 0 .+ zz * 1 .+ pog.offset[3]#spin z coordinates
+  xx = reshape((-FOVx/2:pog.Δx[1]:FOVx/2), :, 1, 1)  #[(-FOVx/2:Δx[1]:FOVx/2)...;]
+  yy = reshape((-FOVy/2:pog.Δx[2]:FOVy/2), 1, :, 1)  #[(-FOVy/2:Δx[2]:FOVy/2)...;;]
+  zz = reshape((-FOVz/2:pog.Δx[3]:FOVz/2), 1, 1, :)  #[(-FOVz/2:Δx[3]:FOVz/2)...;;;]
+  x = xx * 1 .+ yy * 0 .+ zz * 0 .+ pog.offset[1]  #spin x coordinates
+  y = xx * 0 .+ yy * 1 .+ zz * 0 .+ pog.offset[2]  #spin y coordinates
+  z = xx * 0 .+ yy * 0 .+ zz * 1 .+ pog.offset[3]  #spin z coordinates
 
   return KomaMRI.Phantom(
     name=pog.name,
